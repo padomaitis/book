cfd2555a1651d6b48b651d0b7a7c5a48bc36a8cf
p.adomaitis@yahoo.com <p.adomaitis@yahoo.com>
2017-02-12 20:18:27 +0200

    ecs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,14 @@ use Illuminate\Routing\Controller as BaseController;
 class HomeController extends Controller{
 	
 	public function getBookList()
-	{
+		$newGenre = new Genre();
+		$newGenre->GenreName = "Drama";
+		$newGenre->save();
+		$newbook =>new Book();
+		$newbook->Name="Mano";
+		$newbook->ShortDesc="Mano knyga";
+		$newbook->genre_id = 1;
+		$newbook->save();
 		$books = Book::with('genre')->paginate(1);
 	
 		return view('welcome',['books'=>$books]);
