485767d3c499129069cbef08a7537bba0a2b0255
p.adomaitis@yahoo.com <p.adomaitis@yahoo.com>
2017-02-12 20:29:07 +0200

    ecs

--- config/database.php
+++ config/database.php
@@ -46,11 +46,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => 'ec2-54-225-66-44.compute-1.amazonaws.com',
+            'port' => '5432'),
+            'database' => 'd6r3824h08t9tr',
+            'username' => 'ioalppwlqlmzqt',
+            'password' => 'f62a05b310030fbfd900119ea77bea342a53721eaacfa7fe42715b34c757d9c3',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
