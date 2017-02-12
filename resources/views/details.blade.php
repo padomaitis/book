a19927c77d548efe5cb733f171cceb5b5256b2eb
p.adomaitis@yahoo.com <p.adomaitis@yahoo.com>
2017-02-12 23:23:28 +0200

    ecs

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -8,6 +8,7 @@
         </h3>
         <h4>{{$book->Name}}</h4>
         <p>{{$book->LongDesc}}</p>
+		<h4>{{$book->Author}}</h4>
         <a href="" onclick="window.history.back()">Atgal</a>
     </div>
 </div>
