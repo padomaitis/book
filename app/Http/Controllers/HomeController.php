6dfba446ed50a902ec211d9aa3f55534a3f5fc9b
p.adomaitis@yahoo.com <p.adomaitis@yahoo.com>
2017-02-12 20:23:34 +0200

    ecs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,12 +13,14 @@ class HomeController extends Controller{
 	
 	public function getBookList(){
 		$newGenre = new Genre();
+		$newGenre->id = 2;
 		$newGenre->GenreName = "Drama";
 		$newGenre->save();
 		$newbook = new Book();
+		$newbook->BookId=2;
 		$newbook->Name="Mano";
 		$newbook->ShortDesc="Mano knyga";
-		$newbook->genre_id = 1;
+		$newbook->genre_id = 2;
 		$newbook->save();
 		$books = Book::with('genre')->paginate(1);
 	
