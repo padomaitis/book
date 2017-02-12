7a5ef4b58a1ba6470120462a8a924448d047090f
p.adomaitis@yahoo.com <p.adomaitis@yahoo.com>
2017-02-12 23:22:10 +0200

    ecs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,16 +13,7 @@ class HomeController extends Controller{
 	
 	public function getBookList(){
 	 
-		$book = new Book();
-		$book->id=15;
-		$book->Name="Orasas";
-		$book->ShortDesc="Oran";
-		$book->LongDesc="Oransas apie save";
-		$book->ImgSrc="img/dramabook.png";
-		$book->Author="Orandas";
-		$book->year=1999;
-		$book->genre_id=8;
-		$book->save();
+	
 		$books = Book::with('genre')->paginate(5);
 	
 		return view('welcome',['books'=>$books]);
